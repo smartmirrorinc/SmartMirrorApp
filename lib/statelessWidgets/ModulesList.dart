c7559ad251b80d358fe692a206e826fc5f41cdfd
Mathias Tausen <mathias.tausen@gmail.com>
2020-11-24 21:09:52 +0100

    Move and reorder modules w/ drag & drop
    Closes #4.

--- lib/statelessWidgets/ModulesList.dart
+++ lib/statelessWidgets/ModulesList.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:drag_and_drop_lists/drag_and_drop_lists.dart';
 import 'package:smartmirror/modules/Module.dart';
 import 'package:smartmirror/helpers/MmmpServer.dart';
 
@@ -32,6 +33,8 @@ class ModulesList extends StatefulWidget {
 
 class _ModulesListState extends State<ModulesList> {
   Future<List<Module>> futureModules;
+  List<ModulePosition> _modulePositions;
+  Map<ModulePosition, List<Module>> _sortedModules;
 
   @override
   void initState() {
@@ -95,76 +98,162 @@ class _ModulesListState extends State<ModulesList> {
       ),
     );
   }
-}
 
-Widget _buildList(MmmpServer server, List<Module> modules, Function refresh) {
-  return Builder(builder: (context) {
-    // Create map where keys is each populated position and values is the
-    // modules in that position
-    Map<ModulePosition, List<Module>> sortedModules =
-        new Map<ModulePosition, List<Module>>();
-    modules.forEach((Module x) {
-      if (!sortedModules.containsKey(x.position)) {
-        sortedModules[x.position] = new List<Module>();
-      }
-      sortedModules[x.position].add(x);
+  // triggered when reordering a module
+  void _onItemReorder(
+      int oldItemIndex, int oldListIndex, int newItemIndex, int newListIndex) {
+    // fetch module from its old position
+    var module = _sortedModules[_modulePositions[oldListIndex]][oldItemIndex];
+
+    // update position
+    if (module.position != _modulePositions[newListIndex]) {
+      // remove from old position list
+      _sortedModules[module.position].remove(module);
+
+      // insert into new position list at appropriate index
+      module.position = _modulePositions[newListIndex];
+      _sortedModules[module.position].insert(newItemIndex, module);
+    } else {
+      _sortedModules[module.position].remove(module);
+      _sortedModules[module.position].insert(newItemIndex, module);
+    }
+
+    // update orders in position list
+    int order = 100;
+    _sortedModules[module.position].forEach((Module x) {
+      x.order = order;
+      order += 100;
+      widget.server.config.setModule(module: x);
     });
 
-    // Create a card for each populated position
-    List<Card> cards = List<Card>();
-    sortedModules.keys.forEach((ModulePosition p) {
-      // Create a clickable tile for each module in that position
-      List<Widget> tiles = List<Widget>();
-      sortedModules[p].forEach((Module m) {
-        tiles.add(Dismissible(
-            background: Container(
-              color: Colors.red,
-              padding: EdgeInsets.symmetric(horizontal: 20),
-              alignment: AlignmentDirectional.centerStart,
-              child: Icon(
-                Icons.delete,
-                color: Colors.white,
-              ),
-            ),
-            key: Key(m.id.toString()),
-            onDismissed: (direction) {
-              // remove dismissible from tree
-              modules.removeWhere((x) => x.id == m.id);
+    // refresh the view
+    refresh();
+  }
 
-              server.config.deleteModule(module: m).then((x) {
-                refresh(); // refresh view in case a position section should disappear
-              });
-            },
-            child: ListTile(
-              title: Text(m.module),
-              onTap: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                      builder: (context) =>
-                          ModuleOverviewApp(server: server, module: m)),
-                ).then((x) {
-                  // refresh view on return in case a module changes position
-                  refresh();
-                });
-              },
-            )));
+  Widget _buildList(MmmpServer server, List<Module> modules, Function refresh) {
+    var backgroundColor = Color.fromARGB(255, 243, 242, 248);
+
+    return Builder(builder: (context) {
+      // Create map where keys is each populated position and values is the
+      // modules in that position
+      Map<ModulePosition, List<Module>> sortedModules =
+          new Map<ModulePosition, List<Module>>();
+      modules.forEach((Module x) {
+        if (!sortedModules.containsKey(x.position)) {
+          sortedModules[x.position] = new List<Module>();
+        }
+        sortedModules[x.position].add(x);
       });
 
-      // Header for the card
-      String title = (p == null) ? "No position" : modulePositionToString(p);
+      // Keep track of lists for future reference (when reordering)
+      _modulePositions = List<ModulePosition>();
+      _sortedModules = sortedModules;
 
-      // The card contains just one child: a ListTile where the title
-      // contains the header and the subtitle contains a Column of all the
-      // modules in that position
-      ListTile entry = ListTile(
-          title: Text(title, style: TextStyle(fontWeight: FontWeight.bold)),
-          subtitle: Column(children: tiles));
+      // Get list of keys in sortedModules, sorted alphabetically. Objective is
+      // to always show positions in the same order
+      // https://stackoverflow.com/questions/18244545/dart-how-to-sort-maps-keys
+      var sortedKeys = sortedModules.keys.toList()
+        ..sort((a, b) => a.toString().compareTo(b.toString()));
 
-      cards.add(Card(child: Column(children: [entry])));
+      // Create a DragAndDropList for each populated position
+      var lists = List<DragAndDropList>();
+      sortedKeys.forEach((ModulePosition p) {
+        // Keep track of those positions
+        _modulePositions.add(p);
+
+        // Sort modules within a position by their order
+        sortedModules[p].sort((a, b) => a.order.compareTo(b.order));
+
+        // Create a clickable, draggable, dissmissible tile for each module in
+        // that position
+        var items = List<DragAndDropItem>();
+        sortedModules[p].forEach((Module m) {
+          items.add(DragAndDropItem(
+              child: Padding(
+                  padding: EdgeInsets.symmetric(vertical: 8, horizontal: 12),
+                  child: Dismissible(
+                      background: Container(
+                        color: Colors.red,
+                        padding: EdgeInsets.symmetric(horizontal: 20),
+                        alignment: AlignmentDirectional.centerStart,
+                        child: Icon(
+                          Icons.delete,
+                          color: Colors.white,
+                        ),
+                      ),
+                      key: Key(m.id.toString()),
+                      onDismissed: (direction) {
+                        // remove dismissible from tree
+                        modules.removeWhere((x) => x.id == m.id);
+                        server.config.deleteModule(module: m).then((x) {
+                          refresh(); // refresh view in case a position section should disappear
+                        });
+                      },
+                      child: ListTile(
+                        title: Text(m.module),
+                        onTap: () {
+                          // when a tile is clicked, go to the respective
+                          // configuration view
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => ModuleOverviewApp(
+                                    server: server, module: m)),
+                          ).then((x) {
+                            // refresh view on return in case a module changes position
+                            refresh();
+                          });
+                        },
+                      )))));
+        });
+
+        // Header for the list
+        String title = (p == null) ? "No position" : modulePositionToString(p);
+
+        var entry = DragAndDropList(
+            header: Text(title, style: TextStyle(fontWeight: FontWeight.bold)),
+            children: items);
+
+        lists.add(entry);
+      });
+
+      // TODO: figure out how to disable drag and drop on headlines, it does
+      // nothing (and shouldnt)
+      return DragAndDropLists(
+        children: lists,
+        onItemReorder: _onItemReorder,
+        listPadding: EdgeInsets.symmetric(horizontal: 15, vertical: 10),
+        itemDivider: Divider(
+          thickness: 2,
+          height: 2,
+          color: backgroundColor,
+        ),
+        itemDecorationWhileDragging: BoxDecoration(
+          color: Colors.white,
+          boxShadow: [
+            BoxShadow(
+              color: Colors.grey.withOpacity(0.5),
+              spreadRadius: 2,
+              blurRadius: 3,
+              offset: Offset(0, 0), // changes position of shadow
+            ),
+          ],
+        ),
+        listInnerDecoration: BoxDecoration(
+          color: Theme.of(context).canvasColor,
+          borderRadius: BorderRadius.all(Radius.circular(8.0)),
+        ),
+        lastItemTargetHeight: 8,
+        addLastItemTargetHeightToTop: true,
+        lastListTargetSize: 40,
+        dragHandle: Padding(
+          padding: EdgeInsets.only(right: 10),
+          child: Icon(
+            Icons.menu,
+            color: Colors.black26,
+          ),
+        ),
+      );
     });
-
-    // Wrap the cards in a listview for scrollability
-    return ListView(children: cards);
-  });
+  }
 }
