286203bbf3b2b1df8aa61819da7b94dbd50974a1
Mathias Tausen <mathias.tausen@gmail.com>
2021-05-29 14:26:49 +0200

    Use environment variables for API keys
    Avoid hard-coded API keys by pulling them from `.env`. See
    `.env.template` for syntax.
    
    This should not be used for API keys that must be kept secret - it is
    still possible to pull these keys from the application binary, but at
    least they're safe from github crawlers.
    
    A couple of links for future reference:
    
    https://github.com/ByneappLLC/flutter_config/blob/master/doc/ANDROID.md
    https://pub.dev/packages/flutter_config
    https://rammic.github.io/2015/07/28/hiding-secrets-in-android-apps/
    https://stackoverflow.com/a/61964197
    
    Closes #9

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:smartmirror/statelessWidgets/ServerList.dart';
+import 'package:flutter_config/flutter_config.dart';
 
-void main() => runApp(ServerListApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized(); // Required by FlutterConfig
+  await FlutterConfig.loadEnvVariables();
+
+  runApp(ServerListApp());
+}
