a123a4a9a4fd947814d77a1c4bb79bee1913b17e
Mathias Tausen <mathias.tausen@gmail.com>
2021-06-08 22:14:28 +0200

    Quick hack for fancy bottom navigation demo
    Using example from https://github.com/tunitowen/fancy_bottom_navigation
    and custom icons using https://www.fluttericon.com/ (icons by Jones)

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:smartmirror/statelessWidgets/ServerList.dart';
+import 'package:smartmirror/statelessWidgets/FancyModulesList.dart';
 import 'package:flutter_config/flutter_config.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized(); // Required by FlutterConfig
   await FlutterConfig.loadEnvVariables();
 
-  runApp(ServerListApp());
+  //runApp(ServerListApp());
+  runApp(FancyModulesListApp());
 }
---
+++ lib/statelessWidgets/FancyModulesList.dart
@@ -0,0 +1,171 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:fancy_bottom_navigation/fancy_bottom_navigation.dart';
+
+class MyFlutterApp {
+  MyFlutterApp._();
+
+  static const _kFontFam = 'MyFlutterApp';
+  static const String _kFontPkg = null;
+
+  static const IconData middle = IconData(0xe800, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData top = IconData(0xe801, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData bottom = IconData(0xe802, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+}
+
+class SecondPage extends StatefulWidget {
+  @override
+  _SecondPageState createState() => _SecondPageState();
+}
+
+class _SecondPageState extends State<SecondPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Second page"),),
+      body: Center(child: Text("Nothing to see here"),),
+    );
+  }
+}
+
+class FancyModulesListApp extends StatelessWidget {
+
+  FancyModulesListApp({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(home:Scaffold(
+      body: Center(
+        child: ModulesList(),
+      ),
+  ));
+  }
+}
+
+class ModulesList extends StatefulWidget {
+
+  ModulesList({Key key}) : super(key: key);
+
+  @override
+  _ModulesListState createState() => _ModulesListState();
+}
+
+class _ModulesListState extends State<ModulesList> {
+int currentPage = 0;
+
+  GlobalKey bottomNavigationKey = GlobalKey();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Fancy Bottom Navigation"),
+      ),
+      body: Container(
+        decoration: BoxDecoration(color: Colors.white),
+        child: Center(
+          child: _getPage(currentPage),
+        ),
+      ),
+      bottomNavigationBar: FancyBottomNavigation(
+        circleColor: Color.fromARGB(255, 253, 75, 29),
+        inactiveIconColor: Color.fromARGB(255, 253, 75, 29),
+        tabs: [
+          TabData(
+              iconData: MyFlutterApp.top,
+              title: "Top",
+              onclick: () {
+                final FancyBottomNavigationState fState = bottomNavigationKey
+                    .currentState as FancyBottomNavigationState;
+                fState.setPage(2);
+              }),
+          TabData(
+              iconData: MyFlutterApp.middle,
+              title: "Middle",
+              onclick: () => Navigator.of(context)
+                  .push(MaterialPageRoute(builder: (context) => SecondPage()))),
+          TabData(iconData: MyFlutterApp.bottom, title: "Bottom")
+        ],
+        initialSelection: 1,
+        key: bottomNavigationKey,
+        onTabChangedListener: (position) {
+          setState(() {
+            currentPage = position;
+          });
+        },
+      ),
+      drawer: Drawer(
+        child: ListView(
+          children: <Widget>[Text("Hello"), Text("World")],
+        ),
+      ),
+    );
+  }
+
+  _getPage(int page) {
+    switch (page) {
+      case 0:
+        return Column(
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            Text("This is the home page"),
+            RaisedButton(
+              child: Text(
+                "Start new page",
+                style: TextStyle(color: Colors.white),
+              ),
+              color: Theme.of(context).primaryColor,
+              onPressed: () {
+                Navigator.of(context).push(
+                    MaterialPageRoute(builder: (context) => SecondPage()));
+              },
+            ),
+            RaisedButton(
+              child: Text(
+                "Change to page 3",
+                style: TextStyle(color: Colors.white),
+              ),
+              color: Theme.of(context).accentColor,
+              onPressed: () {
+                final FancyBottomNavigationState fState = bottomNavigationKey
+                    .currentState as FancyBottomNavigationState;
+                fState.setPage(2);
+              },
+            )
+          ],
+        );
+      case 1:
+        return Column(
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            Text("This is the search page"),
+            RaisedButton(
+              child: Text(
+                "Start new page",
+                style: TextStyle(color: Colors.white),
+              ),
+              color: Theme.of(context).primaryColor,
+              onPressed: () {
+                Navigator.of(context).push(
+                    MaterialPageRoute(builder: (context) => SecondPage()));
+              },
+            )
+          ],
+        );
+      default:
+        return Column(
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            Text("This is the basket page"),
+            RaisedButton(
+              child: Text(
+                "Start new page",
+                style: TextStyle(color: Colors.white),
+              ),
+              color: Theme.of(context).primaryColor,
+              onPressed: () {},
+            )
+          ],
+        );
+    }
+  }}
